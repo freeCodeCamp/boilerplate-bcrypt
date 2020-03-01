f37c7610095a19e0519bf6cb16410c3241af37d6
Glitch (fshiper-fcc-bcrypt) <none>
2020-03-01 20:06:48 +0000

    🐞🏌 Updated with Glitch

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
 const app         = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -12,13 +13,22 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err) console.log(err);
+  console.log(hash)
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true
+  });
+})
 
 
 //END_ASYNC
 
 //START_SYNC
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 
 
 //END_SYNC
